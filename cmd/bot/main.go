926fb76d5c307a70e257862c870a8335039cd282
DarK7663 <mamutov9876@gmail.com>
2026-05-23 18:42:47 +0300

    Discord Config

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import (
+	"discord/config"
+)
 
+func main() {
+	config.Load()
 }
--- config/config.go
+++ config/config.go
@@ -10,13 +10,34 @@ import (
 	"gorm.io/gorm"
 )
 
+type Config struct {
+	DiscordToken  string
+	GuildID       string
+	CommandPrefix string
+}
+
 var db *gorm.DB
 
-func Config() {
+func Load() (*Config, error) {
 	var err error
 	if err := godotenv.Load(); err != nil {
 		log.Println("Warning: .env file not found, using system env vars")
 	}
+
+	cfg := &Config{
+		DiscordToken:  os.Getenv("DISCORD_TOKEN"),
+		GuildID:       os.Getenv("GUILD_ID"),
+		CommandPrefix: os.Getenv("COMMAND_PREFIX"),
+	}
+
+	if cfg.DiscordToken == "" {
+		return nil, fmt.Errorf("DISCORD_TOKEN не задан в .env файле")
+	}
+
+	if cfg.CommandPrefix == "" {
+		cfg.CommandPrefix = "!"
+	}
+
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
@@ -31,6 +52,6 @@ func Config() {
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
 	}
-
-	DB.AutoMigrate(&model)
+	DB.AutoMigrate(&cfg)
+	return cfg, nil
 }
