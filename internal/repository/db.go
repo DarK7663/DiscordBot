73832f662cf0c6477dca7195da4e928024f21f1a
DarK7663 <mamutov9876@gmail.com>
2026-05-23 17:19:32 +0300

    First

---
+++ internal/repository/db.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Initialize() {
+	var err error
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found, using system env vars")
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		dbHost, dbUser, dbPassword, dbName, dbPort, sslMode)
+
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	DB.AutoMigrate(&model)
+
+}
